a62305d77c0efc2b4f37786878a16963787a73a4
zcomert <zafer.comert@hotmail.com>
2026-04-06 14:49:38 +0300

    MVC has been established.

--- StoreWeb/Program.cs
+++ StoreWeb/Program.cs
@@ -13,5 +13,13 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.MapControllers();
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Varsayılan MVC route yapısı
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
