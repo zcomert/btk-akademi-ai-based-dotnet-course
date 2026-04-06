b9ada7cbb53c4793ab4ef88074880dfe254144da
zcomert <zafer.comert@hotmail.com>
2026-04-06 14:04:06 +0300

    Program.cs has been updated.

--- StoreWeb/Program.cs
+++ StoreWeb/Program.cs
@@ -3,5 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 app.MapControllers();
 app.Run();
