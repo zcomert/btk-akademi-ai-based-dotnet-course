116968d1fdeb32f9285d06fc7770ec3e1abf3521
zcomert <zafer.comert@hotmail.com>
2026-04-07 14:20:11 +0300

    RepositoryContext was updated.

--- StoreWeb/Repositories/RepositoryContext.cs
+++ StoreWeb/Repositories/RepositoryContext.cs
@@ -4,5 +4,10 @@ namespace StoreWeb.Repositories;
 
 public class RepositoryContext : DbContext
 {
-    
+    public DbSet<Product> Products { get; set; }
+    public RepositoryContext(DbContextOptions<RepositoryContext> options) 
+        : base(options)
+    {
+        
+    }
 }
