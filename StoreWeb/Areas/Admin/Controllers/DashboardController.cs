e9bffaf9aadfa8061bfc8b67f870182c5a2f1a99
zcomert <zafer.comert@hotmail.com>
2026-04-08 14:11:52 +0300

    Dashboard has been defined.

---
+++ StoreWeb/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreWeb.Repositories;
+
+namespace StoreWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    private readonly RepositoryContext _context;
+
+    public DashboardController(RepositoryContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var productCount = await _context.Products.CountAsync();
+        var totalInventoryValue = await _context.Products.SumAsync(p => p.Price);
+        var latestProducts = await _context.Products
+            .AsNoTracking()
+            .OrderByDescending(p => p.ProductId)
+            .Take(5)
+            .ToListAsync();
+
+        ViewData["Title"] = "Admin Dashboard";
+        ViewData["ProductCount"] = productCount;
+        ViewData["TotalInventoryValue"] = totalInventoryValue;
+
+        return View(latestProducts);
+    }
+}
